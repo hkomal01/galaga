8e28586be7fa0d9ce13a6266e961286cc8b4bb83
Harshdeep Singh Komal <harsh@harshtop>
2024-03-31 11:38:22 -0400

    example pygame

---
+++ game.py
@@ -0,0 +1,47 @@
+import pygame
+import numpy as np
+
+pygame.init()
+screen = pygame.display.set_mode((2256, 1504))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+nx = np.random.randint(-2000, 2000)
+ny = np.random.randint(-2000, 2000)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+
+    pygame.draw.circle(screen, "white", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+    if keys[pygame.K_SPACE]:
+        player_pos.x += nx * dt
+        player_pos.y += ny * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(240) / 1000
+
+pygame.quit()
